f112d026b5d66ba214d4fcfa8e5b6b41b357bd67
Swantan Barua <work.swantan@gmail.com>
2026-09-17 13:01:43 +0530

    Initial Commit

---
+++ Wander/WanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderApp.swift
+//  Wander
+//
+//  Created by Swantan Barua on 17/09/26.
+//
+
+import SwiftUI
+
+@main
+struct WanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WanderTests/WanderTests.swift
@@ -0,0 +1,19 @@
+//
+//  WanderTests.swift
+//  WanderTests
+//
+//  Created by Swantan Barua on 17/09/26.
+//
+
+import Testing
+@testable import Wander
+
+struct WanderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
